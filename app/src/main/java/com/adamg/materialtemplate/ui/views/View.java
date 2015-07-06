f6a20e8e85cd672494c790ae91f1f6a3c19348b4
Adam Greenberg <adam.m.t.greenberg@gmail.com>
2015-07-05 19:24:23 -0700

    View interface marker

---
+++ app/src/main/java/com/adamg/materialtemplate/ui/views/View.java
@@ -0,0 +1,11 @@
+package com.adamg.materialtemplate.ui.views;
+
+/**
+ * View interface created to abstract the view. Base marker class of the UI piece of the MVP paradigm.
+ * 
+ * @author Adam Greenberg
+ * @version 1 on 6/28/15
+ *          All code under The MIT License (MIT) unless otherwise noted.
+ */
+public interface View {
+}
