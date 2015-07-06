413de0231254591bdf1101a82880ce04c713591e
Adam Greenberg <adam.m.t.greenberg@gmail.com>
2015-07-05 17:45:00 -0700

    Base delegate class, does simple construction in setting every piece up, injecting, and then allowing us to model everything after this via a wrapper in the interface or what have you

---
+++ cloud/src/main/java/com/adamg/materialtemplate/cloud/ICloudDelegate.java
@@ -0,0 +1,13 @@
+package com.adamg.materialtemplate.cloud;
+
+/**
+ * Interface that defines the pieces of the Cloud library
+ *
+ * @author Adam Greenberg
+ * @version 1 on 6/28/15
+ *          All code under The MIT License (MIT) unless otherwise noted.
+ */
+public interface ICloudDelegate {
+
+
+}
