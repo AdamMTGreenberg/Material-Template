1405d435c4cc3844a2cbf4da8d162319d3d03220
Adam Greenberg <adam.m.t.greenberg@gmail.com>
2015-07-05 18:19:22 -0700

    Stub class that allows for us to use the API services created by Retrofit with an example

---
+++ cloud/src/main/java/com/adamg/materialtemplate/cloud/module/ApiServiceModule.java
@@ -0,0 +1,39 @@
+package com.adamg.materialtemplate.cloud.module;
+
+/**
+ * Module that provides the API services as needed and built ready to make a request.
+ *
+ * @author Adam Greenberg
+ * @version 1 on 6/28/15
+ * All code under The MIT License (MIT) unless otherwise noted.
+ */
+
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit.RestAdapter;
+
+@Module(
+        complete = false,
+        library = true
+)
+public class ApiServiceModule {
+
+//    /**
+//     * Module that provides the Service that calls an API
+//     *
+//     * @param restAdapter client provided by
+//     *                    {@link RestApiModule
+//     *                    #provideRestAdapter(retrofit.Endpoint, retrofit.client.Client,
+//     *                    com.markonelifestyle.cloud.api.headers.BaseApiHeaders,
+//     *                    com.markonelifestyle.cloud.api.converter.JacksonConverter)}
+//     * @return the implemented Retrofit Service
+//     */
+//    @Provides
+//    @Singleton
+//    ApiService provideApiService(final RestAdapter restAdapter) {
+//        return restAdapter.create(ApiService.class);
+//    }
+}
